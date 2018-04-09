868defd162c25d3217136dd7f8f9279dbb551ffd
tomonari_t <tomo1208.japan@gmail.com>
2018-04-09 21:25:43 +0900

    linterで整形＆コメント修正

--- src/SemVersion.js
+++ src/SemVersion.js
@@ -1,71 +1,68 @@
 'use strict';
 
 const compareTwoNumber = (a, b) => {
-    if (a > b) return 1;
-    if (a < b) return -1;
-    return 0;
-}
+  if (a > b) return 1;
+  if (a < b) return -1;
+  return 0;
+};
 
-const validate = Symbol('validate')
+const isInteger = num => Number.isInteger(num);
+const isOverZero = num => num >= 0;
+
+const validate = Symbol('validate');
 
 class SemVersion {
-    constructor(major, minor, patch) {
-        if (!this[validate](major, minor, patch)) {
-            throw new TypeError('Invalid Arguments Error');
-        }
-        this._major = major;
-        this._minor = minor;
-        this._patch = patch;
+  constructor(major, minor, patch) {
+    if (!this[validate](major, minor, patch)) {
+      throw new TypeError('Invalid Arguments Error');
     }
+    this._major = major;
+    this._minor = minor;
+    this._patch = patch;
+  }
 
-    toString() {
-        return `${this.major}.${this.minor}.${this.patch}`;
-    }
+  toString() {
+    return `${this.major}.${this.minor}.${this.patch}`;
+  }
 
-    equal(target) {
-        const comparedMajor = compareTwoNumber(this.major, target.major)
-        const comparedMinor = compareTwoNumber(this.minor, target.minor)
-        const comparedPatch = compareTwoNumber(this.patch, target.patch)
-        return comparedMajor === 0 && comparedMinor === 0 && comparedPatch === 0;
-    }
+  equal(target) {
+    const comparedMajor = compareTwoNumber(this.major, target.major);
+    const comparedMinor = compareTwoNumber(this.minor, target.minor);
+    const comparedPatch = compareTwoNumber(this.patch, target.patch);
+    return comparedMajor === 0 && comparedMinor === 0 && comparedPatch === 0;
+  }
 
-    compare(target) {
-        const comparedMajor = compareTwoNumber(this.major, target.major)
-        if (comparedMajor !== 0) {
-            return comparedMajor;
-        }
-        const comparedMinor = compareTwoNumber(this.minor, target.minor)
-        if (comparedMinor !== 0) {
-            return comparedMinor;
-        }
-        const comparedPatch = compareTwoNumber(this.patch, target.patch)
-        if (comparedPatch !== 0) {
-            return comparedPatch;
-        }
-        return 0;
+  compare(target) {
+    for (const key of ['major', 'target', 'patch']) {
+      const compared = compareTwoNumber(this[key], target[key]);
+      if (compared !== 0) return compared;
     }
+    return 0;
+  }
 
-    print() {
-        console.log(this.toString())
-    }
+  print() {
+    console.log(this.toString());
+  }
 
-    get major() {
-        return this._major;
-    }
+  get major() {
+    return this._major;
+  }
 
-    get minor() {
-        return this._minor;
-    }
+  get minor() {
+    return this._minor;
+  }
 
-    get patch() {
-        return this._patch;
-    }
+  get patch() {
+    return this._patch;
+  }
 
-    [validate](major, minor, patch) {
-        const isAllInteger = Number.isInteger(major) && Number.isInteger(minor) && Number.isInteger(patch);
-        const isAllOverZero = major >= 0 && minor >= 0 && patch >= 0;
-        return isAllInteger && isAllOverZero;
-    }
+  [validate](major, minor, patch) {
+    const isAllInteger = isInteger(major) && isInteger(minor) && isInteger(patch);
+    const isAllOverZero = isOverZero(major) && isOverZero(minor) && isOverZero(patch);
+    console.log(isAllInteger);
+    console.log(isAllOverZero);
+    return isAllInteger && isAllOverZero;
+  }
 }
 
 module.exports = SemVersion;
--- test/SemVersion.spec.js
+++ test/SemVersion.spec.js
@@ -1,86 +1,86 @@
-'use strict'
+'use strict';
 
 const assert = require('power-assert');
 const SemVersion = require('../src/SemVersion');
 
 describe('SemVersionクラス', () => {
 
-    describe('インスタンス生成時、major, minor, patch３つの情報をうけとる。', () => {
-        let version;
+  describe('インスタンス生成時、major, minor, patch３つの情報をうけとる。', () => {
+    let version;
 
-        beforeEach('前処理', () => {
-           version = new SemVersion(1, 2, 3);
-        });
-
-        describe('生成に成功するとき、', () => {
-
-            it('SemVersion が生成できる', () => assert.ok(new SemVersion(1, 2, 3)));
-            it('major が 1 を得る', () => assert.equal(version.major, 1));
-            it('minor が 2 を得る', () => assert.equal(version.minor, 2));
-            it('patch が 3 を得る', () => assert.equal(version.patch, 3));
-        });
-
-
-        describe('生成に失敗するとき', () => {
-    
-            it('major: null, minor: undefined, patch [] のとき例外をthrowする', () => {
-                assert.throws(() => new SemVersion(null, undefined, []), TypeError);
-            });
-
-            it('major: 1.1, minor: 1.1, patch 1.1 のとき例外をthrowする', () => {
-                assert.throws(() => new SemVersion(1.1, 1.1, 1.1), TypeError);
-            });
-        });
+    beforeEach('前処理', () => {
+      version = new SemVersion(1, 2, 3);
     });
 
-    describe('インスタンスは文字列として変換できる', () => {
-        let version;
+    describe('生成に成功するとき、', () => {
 
-        before('前処理', () => {
-           version = new SemVersion(1, 2, 3);
-        });
-
-        it('major: 1, minor: 2, patch 3 のとき、"1.2.3" を得る', () => {
-            assert.equal(version.toString(), '1.2.3');
-        });
+      it('SemVersion が生成できる', () => assert.ok(new SemVersion(1, 2, 3)));
+      it('major が 1 を得る', () => assert.equal(version.major, 1));
+      it('minor が 2 を得る', () => assert.equal(version.minor, 2));
+      it('patch が 3 を得る', () => assert.equal(version.patch, 3));
     });
 
-    describe('バージョンオブジェクトは他のバージョンオブジェクトと比較できる', () => {
-        let subject;
 
-        beforeEach('前準備', () => {
-            subject = new SemVersion(1, 2, 3);
-        });
+    describe('生成に失敗するとき', () => {
 
-        describe('バージョンオブジェクトは等価比較ができる', () => {
-            
-            it('Version1.2.3のcompareメソッドにVersion1.2.3のインスタンスを渡されると', () => {
-                const target = new SemVersion(1, 2, 3);
-                assert.equal(subject.equal(target), true);
-            });
-            
-            it('Version1.2.3のcompareメソッドにVersion1.2.5のインスタンスを渡されると', () => {
-                const target = new SemVersion(1, 2, 5);
-                assert.equal(subject.equal(target), false);
-            });
-        });
-        
-        describe('バージョンオブジェクトは大小比較ができる。', () => {
-            
-            it('Version1.2.3のcompareメソッドにVersion4.5.6のインスタンスを渡されると、-1を返す。', () => {
-                const target = new SemVersion(4, 5, 6);
-                assert.equal(subject.compare(target), -1);
-            });
-            
-            it('Version1.2.3のcompareメソッドにVersion1.2.3のインスタンスを渡されると、0を返す。', () => {
-                const target = new SemVersion(1, 2, 3);
-                assert.equal(subject.compare(target), 0);
-            });
-            
-            it('Version1.2.3のcompareメソッドにVersion0.1.2のインスタンスを渡されると、1を返す。', () => {
-                const target = new SemVersion(0, 1, 2);
-                assert.equal(subject.compare(target), 1);
-            });
-        })
+      it('major: null, minor: undefined, patch [] のとき例外をthrowする', () => {
+        assert.throws(() => new SemVersion(null, undefined, []), TypeError);
+      });
+
+      it('major: 1.1, minor: 1.1, patch 1.1 のとき例外をthrowする', () => {
+        assert.throws(() => new SemVersion(1.1, 1.1, 1.1), TypeError);
+      });
     });
+  });
+
+  describe('インスタンスは文字列として変換できる', () => {
+    let version;
+
+    before('前処理', () => {
+      version = new SemVersion(1, 2, 3);
+    });
+
+    it('major: 1, minor: 2, patch 3 のとき、"1.2.3" を得る', () => {
+      assert.equal(version.toString(), '1.2.3');
+    });
+  });
+
+  describe('バージョンオブジェクトは他のバージョンオブジェクトと比較できる', () => {
+    let subject;
+
+    beforeEach('前準備', () => {
+      subject = new SemVersion(1, 2, 3);
+    });
+
+    describe('バージョンオブジェクトは等価比較ができる', () => {
+
+      it('Version1.2.3のcompareメソッドにVersion1.2.3のインスタンスを渡されると', () => {
+        const target = new SemVersion(1, 2, 3);
+        assert.equal(subject.equal(target), true);
+      });
+
+      it('Version1.2.3のcompareメソッドにVersion1.2.5のインスタンスを渡されると', () => {
+        const target = new SemVersion(1, 2, 5);
+        assert.equal(subject.equal(target), false);
+      });
+    });
+
+    describe('バージョンオブジェクトは大小比較ができる。', () => {
+
+      it('Version1.2.3のcompareメソッドにVersion4.5.6のインスタンスを渡されると、-1を返す。', () => {
+        const target = new SemVersion(4, 5, 6);
+        assert.equal(subject.compare(target), -1);
+      });
+
+      it('Version1.2.3のcompareメソッドにVersion1.2.3のインスタンスを渡されると、0を返す。', () => {
+        const target = new SemVersion(1, 2, 3);
+        assert.equal(subject.compare(target), 0);
+      });
+
+      it('Version1.2.3のcompareメソッドにVersion0.1.2のインスタンスを渡されると、1を返す。', () => {
+        const target = new SemVersion(0, 1, 2);
+        assert.equal(subject.compare(target), 1);
+      });
+    });
+  });
 });
